d021122a89dc00fa6bfd8765c0404877de7c4456
ashleyphillips <ashleylphil@gmail.com>
2018-01-24 16:47:04 -0600

    shift key

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#keyboard-upper-container').hide();
+    $(document).keydown(function(e) {
+        if (e.keyCode = 16) {
+            $('#keyboard-upper-container').show();
+            $('#keyboard-lower-container').hide();
+        }})
+    $(document).keyup(function(e) {
+        if (e.keyCode = 16) {
+            $('#keyboard-upper-container').hide();
+            $('#keyboard-lower-container').show();
+        }})
+});
