c90daa0618a207d2b70d715b450d9a6f27733360
ashleyphillips <ashleylphil@gmail.com>
2018-01-25 11:04:01 -0600

    yellow keys

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+
 $(document).ready(function() {
     $('#keyboard-upper-container').hide();
-    $(document).keydown(function(e) {
-        if (e.keyCode = 16) {
-            $('#keyboard-upper-container').show();
-            $('#keyboard-lower-container').hide();
-        }})
-    $(document).keyup(function(e) {
-        if (e.keyCode = 16) {
-            $('#keyboard-upper-container').hide();
-            $('#keyboard-lower-container').show();
-        }})
 });
+
+$(document).keydown(function(e) {
+    if (e.keyCode == 16) {
+        $('#keyboard-upper-container').show();
+        $('#keyboard-lower-container').hide();
+}});
+
+$(document).keyup(function(e) {
+    if (e.keyCode == 16) {
+        $('#keyboard-upper-container').hide();
+        $('#keyboard-lower-container').show();
+}});
+
+$(document).keypress(function (e) {
+    if (e.keyCode >=8) {
+        $('span').css("background-color", "yellow");
+}});
