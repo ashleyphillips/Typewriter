f663aeb460160727f9f6f70d7cad8fa6e933dcbb
ashleyphillips <ashleylphil@gmail.com>
2018-01-25 16:32:38 -0600

    styling

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 
 $(document).ready(function() {
     $('#keyboard-upper-container').hide();
+    $(document).keydown(function(e) {
+        if (e.keyCode == 16) {
+            $('#keyboard-upper-container').show();
+            $('#keyboard-lower-container').hide();
+    }});
+    
+    $(document).keyup(function(e) {
+        if (e.keyCode == 16) {
+            $('#keyboard-upper-container').hide();
+            $('#keyboard-lower-container').show();
+    }});
 });
-
-$(document).keydown(function(e) {
-    if (e.keyCode == 16) {
-        $('#keyboard-upper-container').show();
-        $('#keyboard-lower-container').hide();
-}});
-
-$(document).keyup(function(e) {
-    if (e.keyCode == 16) {
-        $('#keyboard-upper-container').hide();
-        $('#keyboard-lower-container').show();
-}});
 
 $(document).bind('keydown', function(e) {
     $('#' + (e.keyCode)).css('background-color', 'yellow');
