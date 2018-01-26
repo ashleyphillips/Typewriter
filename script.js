7c5b5043fa4a94c1eeeba9dc5b3d2976bc398fa9
ashleyphillips <ashleylphil@gmail.com>
2018-01-26 16:48:56 -0600

    sentences appeared

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
+'Too ato too nOt enot one totA not anot tOO aNot', 
+'oat itain oat tain nate eate tea anne inant nean', 
+'itant eate anot eat nato inate eat anot tain eat', 
+'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
+let sentenceCounter = 0;
+let sentenceContainer = $('#sentence');
+sentenceContainer.append(sentences);
+
+// const splitSentence = sentence.split('');
+
+//     const configuredSentence = splitSentence.map((character, index) => {
+//         return `<span id="target-${index}">${character}</span>`;
+//     }).join('');
+
+//     sentenceContainer.append(configuredSentence);
+    
 
 $(document).ready(function() {
     $('#keyboard-upper-container').hide();
